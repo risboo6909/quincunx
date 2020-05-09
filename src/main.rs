f1a5c6b14c4e5eb260fd059f202a84f60d2125e7
risboo6909 <ttyv00@gmail.com>
2020-05-10 01:49:41 +0300

    Experiment with nphysics, add gravity force

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,38 @@ use kiss3d::camera::{Camera, ArcBall};
 use kiss3d::planar_camera::PlanarCamera;
 use kiss3d::renderer::Renderer;
 use kiss3d::post_processing::PostProcessingEffect;
+use kiss3d::nalgebra as na;
 
-use nalgebra::{Translation3, Point3};
+use ncollide3d::shape::{Ball, ShapeHandle};
+use nphysics3d::world::{DefaultMechanicalWorld, DefaultGeometricalWorld};
+use nphysics3d::object::{DefaultBodySet, ColliderDesc, DefaultColliderSet, RigidBodyDesc, BodyPartHandle, DefaultBodyHandle,
+DefaultColliderHandle};
+use nphysics3d::force_generator::DefaultForceGeneratorSet;
+use nphysics3d::joint::DefaultJointConstraintSet;
+
+pub struct World<N: na::RealField> {
+    mworld: DefaultMechanicalWorld<N>,
+    gworld: DefaultGeometricalWorld<N>,
+    /// The set of physics bodies
+    pub bodies: DefaultBodySet<N>,
+    /// The set of colliders, each attached to a body-part
+    pub colliders: DefaultColliderSet<N>,
+    /// Joint constraints on bodies
+    pub joint_constraints: DefaultJointConstraintSet<N>,
+    /// Forces acting on bodies
+    pub force_generators: DefaultForceGeneratorSet<N>,
+    /// Simulation time point
+    pub time: N,
+}
 
 struct AppState {
     cam: ArcBall,
     c: SceneNode,
+
+    rb_handler: DefaultBodyHandle,
+    col_handler: DefaultColliderHandle,
+    world: World<f32>,
+
     //rot: UnitQuaternion<f32>,
 }
 
@@ -28,40 +54,71 @@ impl State for AppState {
 
     fn step(&mut self, window: &mut Window) {
 
-        for event in window.events().iter() {
-            match event.value {
-                WindowEvent::MouseButton(button, Action::Press, modif) => {
-                    // println!("mouse press event on {:?} with {:?}", button, modif);
-                    //let window_size = nalgebra::Vector2::new(window.size()[0] as f32, window.size()[1] as f32);
-                    //sel_pos = camera.unproject(&last_pos, &window_size);
-                    // println!(
-                    //     "conv {:?} to {:?} win siz {:?} ",
-                    //     last_pos, sel_pos, window_size
-                    // );
+        let pos = self.world.colliders.get(self.col_handler).unwrap().position();
+        self.c.set_local_transformation(*pos);
 
-                }
-                _ => {}
-            }
-        }
+        // self.c.prepend_to_local_translation(&na::Translation3::new(0.0, -0.001, 0.0));
 
-        self.c.prepend_to_local_translation(&Translation3::new(0.0, -0.001, 0.0));
+        self.world.mworld.step(
+            &mut self.world.gworld, 
+            &mut self.world.bodies, 
+            &mut self.world.colliders, 
+            &mut self.world.joint_constraints, 
+            &mut self.world.force_generators
+        );
     }
 }
 
 fn main() {
-    let eye = Point3::new(1f32, 1.0, 1.0);
-    let at = Point3::origin();
+    
+    /*
+     * World
+     */
+    let mut mworld = DefaultMechanicalWorld::new(na::Vector3::new(0.0, -9.81, 0.0));
+    let mut gworld = DefaultGeometricalWorld::new();
+    let mut bodies = DefaultBodySet::new();
+    let mut colliders = DefaultColliderSet::new();
+    let mut joint_constraints = DefaultJointConstraintSet::new();
+    let mut force_generators = DefaultForceGeneratorSet::new();
+
+    let eye = na::Point3::new(1f32, 1.0, 1.0);
+    let at = na::Point3::origin();
 
     let mut window = Window::new("Quincunx");
 
+    // camera
     let arc_ball = ArcBall::new(eye, at);
-    let mut c = window.add_sphere(0.01);
+    
+    // sphere
+    let mut c = window.add_sphere(0.1);
+    
+    // build the rigid body
+    let rb = RigidBodyDesc::new().translation(na::Vector3::new(0.0, 0.0, 0.0)).build();
+    let rb_handle = bodies.insert(rb);
+
+    // build the collider
+    let co = ColliderDesc::new(ShapeHandle::new(Ball::new(0.1)));
+    let col_handle = colliders.insert(co.density(1.0).build(BodyPartHandle(rb_handle, 0)));
 
     c.set_color(1.0, 0.0, 0.0);
-    c.set_local_translation(Translation3::new(0.0, 0.0, 0.0));
+    c.set_local_translation(na::Translation3::new(0.0, 0.0, 0.0));
 
     window.set_light(Light::StickToCamera);
-    let state = AppState { c, cam: arc_ball };
+
+    mworld.step(&mut gworld, &mut bodies, &mut colliders, &mut joint_constraints, &mut force_generators);
+
+    let state = AppState { c, cam: arc_ball, 
+        rb_handler: rb_handle,
+        col_handler: col_handle,
+        world: World{
+        mworld,
+        gworld,
+        bodies,
+        colliders,
+        joint_constraints,
+        force_generators,
+        time: 0.0,
+    } };
 
     window.render_loop(state)
 }
